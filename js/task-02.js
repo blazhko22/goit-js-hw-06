95e2bc1bec3b8c1f46a71c53949b3435fa39e6cd
blazhko22 <blazhko22@gmail.com>
2021-11-07 15:33:16 +0200

    таск 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,42 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const potatoesEl = document.createElement("li");
+potatoesEl.textContent = "Potatoes";
+potatoesEl.classList.add("item");
+
+const mushroomsEl = document.createElement("li");
+mushroomsEl.textContent = "Mushrooms";
+mushroomsEl.classList.add("item");
+
+const garlicEl = document.createElement("li");
+garlicEl.textContent = "Garlic";
+garlicEl.classList.add("item");
+
+const tomatosEl = document.createElement("li");
+tomatosEl.textContent = "Tomatos";
+tomatosEl.classList.add("item");
+
+const herbsEl = document.createElement("li");
+herbsEl.textContent = "Herbs";
+herbsEl.classList.add("item");
+
+const condimentsEl = document.createElement("li");
+condimentsEl.textContent = "Condiments";
+condimentsEl.classList.add("item");
+
+const ingredientsEl = document.querySelector("ul#ingredients");
+
+ingredientsEl.append(
+  potatoesEl,
+  mushroomsEl,
+  garlicEl,
+  tomatosEl,
+  herbsEl,
+  condimentsEl
+);
