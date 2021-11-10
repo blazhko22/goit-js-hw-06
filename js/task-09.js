d792d616369d6bca7e6f2e896bf5be3e0ed6009a
blazhko22 <blazhko22@gmail.com>
2021-11-11 00:01:49 +0200

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const bodyColor = document.querySelector("body");
+const buttonEl = document.querySelector("button.change-color");
+const textEl = document.querySelector("span.color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+buttonEl.addEventListener("click", (event) => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  textEl.textContent = getRandomHexColor();
+});
