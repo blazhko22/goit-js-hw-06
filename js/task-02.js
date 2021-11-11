f1f26c5a1031cb2e60f97ff9ac16ccdf45f10374
blazhko22 <blazhko22@gmail.com>
2021-11-11 20:06:28 +0200

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,37 +6,16 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-const potatoesEl = document.createElement("li");
-potatoesEl.textContent = "Potatoes";
-potatoesEl.classList.add("item");
-
-const mushroomsEl = document.createElement("li");
-mushroomsEl.textContent = "Mushrooms";
-mushroomsEl.classList.add("item");
-
-const garlicEl = document.createElement("li");
-garlicEl.textContent = "Garlic";
-garlicEl.classList.add("item");
-
-const tomatosEl = document.createElement("li");
-tomatosEl.textContent = "Tomatos";
-tomatosEl.classList.add("item");
-
-const herbsEl = document.createElement("li");
-herbsEl.textContent = "Herbs";
-herbsEl.classList.add("item");
-
-const condimentsEl = document.createElement("li");
-condimentsEl.textContent = "Condiments";
-condimentsEl.classList.add("item");
 
 const ingredientsEl = document.querySelector("ul#ingredients");
 
-ingredientsEl.append(
-  potatoesEl,
-  mushroomsEl,
-  garlicEl,
-  tomatosEl,
-  herbsEl,
-  condimentsEl
-);
+const elements = ingredients.map((element) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.textContent = element;
+  ingredientEl.classList.add("item");
+
+  return ingredientEl;
+});
+console.log(elements);
+
+ingredientsEl.append(...elements);
