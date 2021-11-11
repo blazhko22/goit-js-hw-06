a650eb03465cc056399246310460fd7212dfd21f
blazhko22 <blazhko22@gmail.com>
2021-11-11 20:49:20 +0200

    task-5

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,13 @@
-const refs = {
-  input: document.querySelector("input#name-input"),
-  nameInput: document.querySelector("span#name-output"),
-};
+const input = document.querySelector("input#name-input");
+let nameInput = document.querySelector("span#name-output");
 
-refs.input.addEventListener("input", onInputText);
+input.addEventListener("input", onInputText);
 
 function onInputText(event) {
   console.log(event.currentTarget.value);
-  refs.nameInput.textContent = event.currentTarget.value;
+  if (input.value === "") {
+    nameInput.textContent = "Anonymous";
+  } else {
+    nameInput.textContent = event.currentTarget.value;
+  }
 }
